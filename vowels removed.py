397d4d86102b87758a91d7de61be26df48975548
shyam6053 <150471474+shyam6053@users.noreply.github.com>
2023-11-10 23:51:01 +0530

    3.. string with all vowels removed
    3. write a string function with all vowels removed

---
+++ vowels removed.py
@@ -0,0 +1,16 @@
+print("enter the word")
+s=str(input())
+for i in s:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
+        print("",end="")
+    else:
+        print(i,end="")
+    
+           
+
+
+
+
+
+
+
